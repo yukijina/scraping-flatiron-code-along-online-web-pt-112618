96f19ca8acd341205055fa07f41ef815f3f5dff7
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-14 23:41:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,10 @@ class Scraper
   
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry
+    
+    doc.css(".post").each do |post|
+      
+    end 
   end 
   
     def print_courses
